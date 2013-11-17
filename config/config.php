9d4c676c0a365421106353a8ac8b18ae011d64e5
owsx <owsx@163.com>
2013-11-18 04:17:41 +0800

    Create config.php

---
+++ config/config.php
@@ -0,0 +1,20 @@
+<?php
+
+return array(
+
+'extend' => 'init',
+
+'uri.default_group' => 'home',
+
+'database' => 'db',
+'db' => array(
+	'type' => 'mysql',
+	'addr' => '127.0.0.1:3306',
+	'user' => 'root',
+	'pass' => '123456',
+	'name' => 'g',
+),
+
+
+
+);
