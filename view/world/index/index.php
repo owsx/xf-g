af2fa690e2e61776c86581a9a97cae3e1ac036a6
owsx <owsx@163.com>
2013-11-18 04:31:03 +0800

    Create index.php

---
+++ view/world/index/index.php
@@ -0,0 +1,11 @@
+<?=$this->load('header')?>
+
+<div id="tool">
+<a href="<?=U('char/info')?>"><?=L('char_info')?></a>
+<a href="<?=U('char/skill')?>"><?=L('char_skill')?></a>
+<a href="<?=U('map/info')?>"><?=L('map_info')?></a>
+
+</div>
+
+
+<?=$this->load('footer')?>
